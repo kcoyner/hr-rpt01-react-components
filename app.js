0891f26a82867be2b9f961ee4a8d56cffe07a3cf
Kevin Coyner <rustybear@gmail.com>
2017-07-07 16:24:54 -0400

    Kevins-iMac.home: Kevins-iMac.home: Completed through 'Class components'

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
-const GroceryListItem = (props) => {
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  var onListItemClick = (event) => {
+  render() {
+    return (
+      <li key={this.props.key} onClick={onListItemClick}>{this.props.item}</li>
+    );
+  }
+
+}
+
+var onListItemClick = (event) => {
     console.log('I got clicked');
-  };
+};
 
-  return (<ul>
-    {props.groceryItems.map(function (item) {
-      return [<li onClick={onListItemClick}>{item}</li>];
-    })}
+
+const GroceryList = (props) => {
+  return (
+    <ul>
+      <h2>Grocery List</h2>
+        {items.map((item, key) =>
+          <GroceryListItem item={item} key={key} />
+        )}
     </ul>
   );
 }
 
-const GroceryList = () => {
-  var items = ['cucumber', 'kale'];
-  return (
-    <div>
-      <h2>Grocery List</h2>
-        <GroceryListItem groceryItems={items} />
-    </div>
-  );
-}
+var items = ['cucumber', 'kale', 'daikon'];
 
-ReactDOM.render(<GroceryList />, document.getElementById('app'));
+ReactDOM.render(<GroceryList props={items} />, document.getElementById('app'));
 
 
 
+
+// const GroceryListItem = (props) => {
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   };
+//   return (<ul>
+//     {props.groceryItems.map(function (item) {
+//       return [<li onClick={onListItemClick}>{item}</li>];
+//     })}
+//     </ul>
+//   );
+// }
