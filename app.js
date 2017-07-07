58929e961119614bf28ecfa9f3e9ffa51d6a4180
Kevin Coyner <rustybear@gmail.com>
2017-07-07 15:17:29 -0400

    Kevins-iMac.home: Kevins-iMac.home: Completed 'Component Properties aka props'

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
+const GroceryListItems = (props) => {
+  return (<ul>
+    {props.groceryItems.map(function (item) {
+      return [<li>{item}</li>];
+    })}
+    </ul>
+  );
+
+  // return (
+  //       <li>{props.items[0]}</li>
+  //       <li>{props.items[1]}</li>
+  // );
+
+}
+
 const GroceryList = () => {
+  var items = ['cucumber', 'kale'];
   return (
     <div>
-      <ul>
-        <Cucumbers />
-        <Kale />
-      </ul>
+      <h2>Grocery List</h2>
+        <GroceryListItems groceryItems={items} />
     </div>
   );
 }
 
-const Kale = () => {
-  return (
-    <li>kale</li>
-  );
-}
-
-const Cucumbers = () => {
-  return (
-    <li>cucumbers</li>
-  );
-}
-
-
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
 
 
+// <GroceryListItems items={['cucumbers', 'kale']} />
 
