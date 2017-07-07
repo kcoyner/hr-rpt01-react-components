7c9064464255ed51425c037ec9a391d75caf4478
Kevin Coyner <rustybear@gmail.com>
2017-07-07 16:37:20 -0400

    Kevins-iMac.home: Kevins-iMac.home: onClick working within the Class.

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ class GroceryListItem extends React.Component {
     super(props);
   }
 
+  onListItemClick () {
+    console.log('I got clicked');
+  }
+
   render() {
     return (
-      <li key={this.props.key} onClick={onListItemClick}>{this.props.item}</li>
+      <li key={this.props.key} onClick={event => this.onListItemClick()}>{this.props.item}</li>
     );
   }
 
-}
 
-var onListItemClick = (event) => {
-    console.log('I got clicked');
-};
+}
 
 
 const GroceryList = (props) => {
