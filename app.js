797af6fdbce0ad31bb1e37723a3173d303c7020d
Kevin Coyner <rustybear@gmail.com>
2017-07-07 14:34:12 -0400

    Kevins-iMac.home: Kevins-iMac.home: Completed 'Creating and rendering nested React components'

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const GroceryList = () => {
+  return (
+    <div>
+      <ul>
+        <Cucumbers />
+        <Kale />
+      </ul>
+    </div>
+  );
+}
+
+const Kale = () => {
+  return (
+    <li>kale</li>
+  );
+}
+
+const Cucumbers = () => {
+  return (
+    <li>cucumbers</li>
+  );
+}
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
+
+
+
